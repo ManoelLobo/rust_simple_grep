7898d1763598804f5b4e2613b342ff07c6372bc9
Manoel <manoel.lobo@gmail.com>
2022-08-11 17:46:11 -0300

    refactor: parse config as function

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Search query: {query}");
     println!("File (path): {file_path}");
@@ -12,4 +11,11 @@ fn main() {
     let file_content = fs::read_to_string(file_path).expect("Could not read file");
 
     println!("File content:\n{file_content}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
