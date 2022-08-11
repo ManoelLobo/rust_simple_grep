ef0734ba7ff716a05abb3f5531b4c768429f2db1
Manoel <manoel.lobo@gmail.com>
2022-08-11 20:21:07 -0300

    feat: error out to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Argument error: {err}");
+        eprintln!("Argument error: {err}");
         process::exit(1);
     });
 
     if let Err(e) = simplegrep::run(config) {
-        println!("App error: {e}");
+        eprintln!("App error: {e}");
         process::exit(1);
     };
 }
