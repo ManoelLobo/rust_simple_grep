3ab91069981a06cc2550eb865247242f0d861abf
Manoel <manoel.lobo@gmail.com>
2022-08-11 17:50:39 -0300

    refactor: Config struct

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Search query: {query}");
-    println!("File (path): {file_path}");
+    println!("Search query: {}", config.query);
+    println!("File (path): {}", config.file_path);
 
-    let file_content = fs::read_to_string(file_path).expect("Could not read file");
+    let file_content = fs::read_to_string(config.file_path).expect("Could not read file");
 
     println!("File content:\n{file_content}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
