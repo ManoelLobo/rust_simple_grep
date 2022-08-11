766d8537f62c49ff2d06bfa4c7275ab9fa654490
Manoel <manoel.lobo@gmail.com>
2022-08-11 17:41:02 -0300

    feat: reading file content

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +8,8 @@ fn main() {
 
     println!("Search query: {query}");
     println!("File (path): {file_path}");
+
+    let file_content = fs::read_to_string(file_path).expect("Could not read file");
+
+    println!("File content:\n{file_content}");
 }
