1088a319bba8bb9fe8b011a727812ae00e2f4ac8
Manoel <manoel.lobo@gmail.com>
2022-08-11 18:14:12 -0300

    refactor: moved lib content to own file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use simplegrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,35 +13,9 @@ fn main() {
     println!("Search query: {}", config.query);
     println!("File (path): {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = simplegrep::run(config) {
         println!("App error: {e}");
 
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let file_content = fs::read_to_string(config.file_path)?;
-
-    println!("File content:\n{file_content}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Missing arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
