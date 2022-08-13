7379206b900791bf0cc6272e9a406607ef25923e
Manoel <manoel.lobo@gmail.com>
2022-08-13 17:11:06 -0300

    refactor: search functions with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -48,29 +48,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut result = vec![];
-
-    for line in content.lines() {
-        if line.contains(query) {
-            result.push(line)
-        }
-    }
-
-    result
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
 
-    let mut result = vec![];
-
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line)
-        }
-    }
-
-    result
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
